868d657dab129b54e90a4b3f1e0d9433df872be4
Scorpion-rep <86839837+Scorpion-rep@users.noreply.github.com>
2021-10-05 16:53:44 +0800

    add a function in js

--- gameDeal.js
+++ gameDeal.js
@@ -1,8 +1,48 @@
 //Variables
 var searchBtn = document.getElementById("searchButton");
 var userInput = document.getElementById("userInput");
+var searchResult = document.getElementById("search-result");
 
 
+// 
+
+
+async function catchApideals() {
+    const response = await fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15")
+    const json = await response.json();
+    document.getElementById("search-result").textContent = URL.createObjectURL(blob)
+}
+
+catchApideals()
+    .then(response =>{
+        console.log("yay");
+    })
+    .catch(error => {        
+        console.log("error");
+        console.error(error);
+});
+
+
+
+
+// function to get the games 
+fetch("https://www.cheapshark.com/api/1.0/games?ids=128,129,130")
+    .then(response => {
+
+        console.log(response);
+        return response.blob();
+    })
+    .then(response => {
+        console.log(response);
+    })
+    .catch(error => {
+        console.log("error!");
+        console.error("error")
+    });
+//const userInputEl = async () => {
+//    const response = await fetch("https://www.cheapshark.com/api/1.0/games?title=batman&steamAppID=35140&limit=60&exact=0.jason";
+//    const myJson = await response.json();
+//}
 // results to be put in heading elements (innerHTML)
 // using Catch to get the errors
 
@@ -22,14 +62,13 @@ var requestOptions = {
     redirect: "follow"
 }
 
-// function to get the games 
+
+
+
+
 function getGames() {
    // 1. Use Fetch to get the responses
-    fetch("https://www.cheapshark.com/api/1.0/games?title=batman&steamAppID=35140&limit=60&exact=0", requestOptions)
-    .then(response => response.text()
-    .then(result => console.log(result))
-    .catch(error => console.log("error". error)));
-
+   
 
 }
 
@@ -38,19 +77,12 @@ var requestOptions = {
     redirect: "follow"
 };
 
+
 // Function search API showing the list of game deals
-
-function getDeals() {
     // 1. Use Fetch to get the responses
-    fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15", requestOptions)
-    .then(response => response.text()
-    .then(result => console.log(result))
-    .catch(error => console.log("error", error)));
-
-}
 
 
-// Tested the responses from API for List of Deals
+    
 
 
 // set up div card ?
@@ -66,23 +98,7 @@ function getDeals() {
 // 2. var for searchInput 
 // loop throu searchInput to check if it's not true/error
 
-// function handleSearchSubmit
-function handleSearchFormSubmit(event) {
-    // 1. event.preventDefault
-    event.preventDefault();
-    // 2. variables for the input element
-    var searchInputEl = document.getElementById("userInput")
 
-    // run a loop to catch error, ensure return valued input
-    if (!searchInputEl) {
-        console.error("need valued input!");
-        return;
-    }
- // set query from API CheapShark (query string/path after endpoint)
-    var queryEl = "https:/www.cheapshark.com/api/1.0/games?title=" + searchInputEl + "&steamAppID=35140&limit=60&exact=0"
-   // location assign 
- location.assign(queryEl)
-}
 
 // add eventListener to search 
-searchBtn.addEventListener("submit", handleSearchFormSubmit);
+//searchBtn.addEventListener("submit", handleSearchForm);
