2ea3867124d5fb14e5f42f1fcf91099179f4a7b2
Siddy <dancore99@outlook.com>
2021-10-07 21:45:25 +0800

    all loads but needs local storage fix

--- gameDeal.js
+++ gameDeal.js
@@ -50,16 +50,23 @@ function listGame(){
     buttonEl.textContent = "More info and deals";
     buttonEl.setAttribute("id", "deals");
     buttonEl.setAttribute("type", "submit");
+    buttonEl.setAttribute("data-title", result[i].external);
     buttonEl.classList.add("btn");
-    menu.appendChild(buttonEl)
+    menu.appendChild(buttonEl);
+    buttonEl.addEventListener("click", function(event){
+        event.preventDefault();
+        var titleTest = event.target.getAttribute("data-title")
+        menu.innerHTML = ""
+        fetchData(titleTest)
+    })
 
     }
     }
 
 };
 
-function fetchData() {
-    fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15")
+function fetchData(titleI) {
+    fetch(`https://www.cheapshark.com/api/1.0/deals?title=${titleI}`)
     .then(response => {
         // check the response for error
         if(!response.ok) {
@@ -74,7 +81,7 @@ function fetchData() {
             // create div to store the results
             return `
                               
-             <div class="deal">                
+             <div id="deal" class="deal">                
                 <p><img src="${deals.thumb} alt="${deals.title}" /> </p>
                 <h1 class="title" > ${deals.title} </h1>                          
                 <p> Normal Price : $ ${deals.normalPrice} </p>
@@ -89,37 +96,48 @@ function fetchData() {
         }).join("");
         
         console.log(html);
+        
         document.getElementById("search-result").insertAdjacentHTML("afterbegin", html);
-
+        
     })
     .catch(error => {
         console.log(error);
     });
 }
+function addHistory() {
+    // Retreive the game name
+    var game = userInput.value
 
-fetchData();
+    // Create list items
+    var newSearchLi = document.createElement("li")
+    var newSearchText = document.createElement("a")
+    newSearchLi.className = "history"
 
+    // Add game name to list item
+    newSearchText.innerHTML = game
 
+    // Add game name to saved search history array
+    savedSearchHistory.push(game)
+    // Save array as string in local storage
+    localStorage.setItem("savedSearches", savedSearchHistory.toString());
+}
 
-var requestOptions = {
-    method: "GET",
-    redirect: "follow"
-};
-
+//activate search button
 searchBtn.addEventListener("click", function(event){
-        event.preventDefault();
-        if (typeof(Storage) !== "undefined") {
+    event.preventDefault();
+    if (typeof(Storage) !== "undefined") {
             // Store
-            localStorage.setItem("saveInput", userInput.value);
+        localStorage.setItem("saveInput", userInput.value);
             // Retrieve
-            document.getElementById("userInput").innerHTML = localStorage.getItem("saveInput")
+            
           } 
-           
-        listGame();
+          
+    listGame();
+    
 
-        console.log(userInput.value)
-     if (!searchInputEl) {
+    console.log(userInput.value)
+     /*if (!searchInputEl) {
         console.error("need valued input!");
         return;
-    }
+    }*/
 })
