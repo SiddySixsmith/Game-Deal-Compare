ada414577101244a5b0adacde257763a4023687c
SiddySixsmith <86714782+SiddySixsmith@users.noreply.github.com>
2021-10-07 19:51:53 +0800

    Update gameDeal.js

--- gameDeal.js
+++ gameDeal.js
@@ -58,6 +58,48 @@ function listGame(){
 
 };
 
+function fetchData() {
+    fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15")
+    .then(response => {
+        // check the response for error
+        if(!response.ok) {
+            console.log(response);
+            throw Error("ERROR")  
+        }
+        return response.json();        
+    })
+    .then(data => {
+        console.log(data);
+        const html = data.map(deals => {
+            // create div to store the results
+            return `
+                              
+             <div class="deal">                
+                <p><img src="${deals.thumb} alt="${deals.title}" /> </p>
+                <h1 class="title" > ${deals.title} </h1>                          
+                <p> Normal Price : $ ${deals.normalPrice} </p>
+                <p> Sale Price : $ ${deals.salePrice} </p>
+                <p> Rating : ${deals.steamRatingPercent} %</p>
+                <p> Deal Rating : ${deals.dealRating} </p>
+                <a class="link" href="https://www.cheapshark.com/redirect?dealID={id}">Follow this link to see more</a>
+             </div> `
+             
+            ;
+
+        }).join("");
+        
+        console.log(html);
+        document.getElementById("search-result").insertAdjacentHTML("afterbegin", html);
+
+    })
+    .catch(error => {
+        console.log(error);
+    });
+}
+
+fetchData();
+
+
 
 var requestOptions = {
     method: "GET",
