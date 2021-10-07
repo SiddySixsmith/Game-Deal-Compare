fa6dcf018c31006d17d689c5e4654dce096a2de2
Siddy <dancore99@outlook.com>
2021-10-07 19:43:48 +0800

    mix and mash

--- gameDeal.js
+++ gameDeal.js
@@ -1,79 +1,79 @@
 //Variables
 var searchBtn = document.getElementById("searchButton");
 var menu = document.getElementById("menu-ul");
-var menu = document.getElementById("menu-ul");
-var userInputEl = document.getElementById("userInput");
-var searchResult = document.getElementById("search-result");
+var userInput = document.getElementById("userInput")
+var saveInput = userInput.value;
+var FormEl = document.getElementById("user-form");
 
+var formdata = new FormData();
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+};
 
+//function to fetch
+function listGame(){
 
-
-// function fetch second Api- deals of the games
-
-function fetchData() {
-    fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15")
-    .then(response => {
-        // check the response for error
-        if(!response.ok) {
-            console.log(response);
-            throw Error("ERROR")  
-        }
-        return response.json();        
-    })
-    .then(data => {
-        console.log(data);
-        const html = data.map(deals => {
-            // create div to store the results
-            return `
-                              
-             <div class="deal">                
-                <p><img src="${deals.thumb} alt="${deals.title}" /> </p>
-                <h1 class="title" > ${deals.title} </h1>                          
-                <p> Normal Price : $ ${deals.normalPrice} </p>
-                <p> Sale Price : $ ${deals.salePrice} </p>
-                <p> Rating : ${deals.steamRatingPercent} %</p>
-                <p> Deal Rating : ${deals.dealRating} </p>
-                <a class="link" href="https://www.cheapshark.com/redirect?dealID={id}">Follow this link to see more</a>
-             </div> `
-             
-            ;
-
-        }).join("");
-        
-        console.log(html);
-        document.getElementById("search-result").insertAdjacentHTML("afterbegin", html);
-
-    })
-    .catch(error => {
-        console.log(error);
+    fetch(`https://www.cheapshark.com/api/1.0/games?title=${userInput.value}&limit=20&exact=0`, requestOptions)
+    .then(test)
+    .catch(function(error) {
+        // handle the error
     });
-}
+    async function test(response) {
+        // handle the response
+       var result = await response.json()
+        console.log(result);
 
-fetchData();
+     menu.innerHTML = ""
+        //creates all required fields for info
+    for (i = 0; i <result.length; i++) {
+       
+        //creates image El
+    var img = document.createElement("img")
+    img.src = result[i].thumb;
+    menu.appendChild(img);
+        
+        // Creates li element for title
+    var li = document.createElement("li");
+    li.textContent = result[i].external;
+    li.setAttribute("id", "title")
+    menu.appendChild(li);
 
+        // Creates li element for cheapest price
+    var li = document.createElement("li");
+    li.textContent = "Cheapest $" + result[i].cheapest;
+    li.setAttribute("id", "cheap")
+    menu.appendChild(li);
 
-var searchFormEl = document.getElementById("user-form")
+        //Creates button element to bring up deals
+    var buttonEl = document.createElement("button");
+    buttonEl.textContent = "More info and deals";
+    buttonEl.setAttribute("id", "deals");
+    buttonEl.setAttribute("type", "submit");
+    buttonEl.classList.add("btn");
+    menu.appendChild(buttonEl)
 
-function handleSearchSubmit(event) {
-    event.preventDefault();
-            
-    var userInputEl = document.getElementById("userInput").value;  
-    
-    if (!userInputEl) {
-        console.error("search again");
-        return;
     }
-    
-    var queryStr = "https://www.cheapshark.com/api/1.0/games?title=" + userInputEl + "&steamAppID=" + "&limit=60&exact=0"
-
-    location.assign(queryStr);
-
-
-}
-
-
-
-    if (!searchInputEl) {
-        console.error("need valued input!");
-        return;
     }
+
+};
+
+
+var requestOptions = {
+    method: "GET",
+    redirect: "follow"
+};
+
+searchBtn.addEventListener("click", function(event){
+        event.preventDefault();
+        if (typeof(Storage) !== "undefined") {
+            // Store
+            localStorage.setItem("saveInput", userInput.value);
+            // Retrieve
+            document.getElementById("userInput").innerHTML = localStorage.getItem("saveInput")
+          } 
+           
+        listGame();
+
+        console.log(userInput.value)
+})
